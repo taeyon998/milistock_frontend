797b404e775234cc4b1b4e4d429bcf2e8df7e134
Taeyon Kim <taeyon998@gmail.com>
2023-08-01 12:20:47 +0000

    dfdsa

---
+++ mili-stock/src/App.tsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const [connection, setConnection] = useState<string>('');
+
+  const connectionTest = () => {
+    axios.get('https://8080-taeyon998-milistockback-7lm6ecztgrx.ws-us102.gitpod.io/').then((response) => {
+      setConnection(response.data);
+    }).catch((error) => {
+      setConnection(error.message);
+    })
+  }
+
+  useEffect(() => {
+    connectionTest();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{connection}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
