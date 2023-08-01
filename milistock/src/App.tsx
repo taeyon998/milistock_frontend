c7c22e580607cd0aa8f942df12d6089f63d9e220
Taeyon Kim <taeyon998@gmail.com>
2023-08-01 11:16:29 +0000

    d

--- milistock/src/App.tsx
+++ milistock/src/App.tsx
@@ -1,29 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   
+  const [connection, setConnection] = useState<string>('');
+
   const connectionTest = () => {
-    axios.get('').then()
+    axios.get('').then((response) => {
+      setConnection(response.data);
+    }).catch((error) => {
+      setConnection(error.message);
+    })
   }
     
+  useEffect(() => {
+    connectionTest();
+  }, []);
   
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" />        
+        <p>{connection}</p>
       </header>
     </div>
   );
