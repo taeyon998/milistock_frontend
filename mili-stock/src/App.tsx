06fb80b4ae2c503261069af477d9abeb2d48fb6b
Taeyon Kim <taeyon998@gmail.com>
2023-08-07 12:24:31 +0000

    Post 회원정보

--- mili-stock/src/App.tsx
+++ mili-stock/src/App.tsx
@@ -3,28 +3,17 @@ import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
+import SignUp from './views/SignUp/index';
+
 function App() {
-
-  const [connection, setConnection] = useState<string>('');
-
-  const connectionTest = () => {
-    axios.get('https://8080-taeyon998-milistockback-7lm6ecztgrx.ws-us102.gitpod.io/').then((response) => {
-      setConnection(response.data);
-    }).catch((error) => {
-      setConnection(error.message);
-    })
-  }
-
-  useEffect(() => {
-    connectionTest();
-  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{connection}</p>
+        
       </header>
+      <SignUp />
     </div>
   );
 }
---
+++ mili-stock/src/views/SignUp/index.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import axios from "axios"
+
+export default function SignUp() {
+    const [requestResult, setRequestResult] = useState<string>('');
+
+    const signUpHandler = () => {
+        const data = {
+            "userEmail": "taeyon@naver.com",
+
+            "userPassword": "awer1234",
+
+            "userPasswordCheck": "awer1234",
+
+            "userNickname": "Hogwart Kim",
+
+            "userPhoneNumber": "010-1111-1111",
+
+            "userAddress": "서울시",
+
+            "userAddressDetail": "서초구"
+        }
+        axios
+            .post('https://8080-taeyon998-milistockback-1jw7vs2iquc.ws-us102.gitpod.io/api/auth/signUp', data)
+            .then((response) => {
+                setRequestResult('Success!');
+            })
+            .catch((error) => {
+                setRequestResult('Failed!');
+            })
+    }
+    return (
+        <div>
+            <h3>{requestResult}</h3>
+            <button onClick={() => signUpHandler()}>회원가입</button>
+        </div>
+    )
+}
