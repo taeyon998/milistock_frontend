230f4d71471f32e2171a72aa09c414c0fd6428c5
Taeyon Kim <taeyon998@gmail.com>
2023-08-08 14:05:34 +0000

    d

--- mili-stock/src/App.tsx
+++ mili-stock/src/App.tsx
@@ -1,20 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import logo from './logo.svg';
 import './App.css';
-
-import SignUp from './views/SignUp/index';
+import Authentication from './views/Authentication';
 
 function App() {
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-      </header>
-      <SignUp />
-    </div>
+    <Authentication />
   );
 }
 
