1aa5e331f604628f383d5fec597eebe0c143a7e9
Taeyon Kim <taeyon998@gmail.com>
2023-08-19 12:27:08 +0000

    dsfsa

--- mili-stock/src/views/Authentication/SignIn/index.tsx
+++ mili-stock/src/views/Authentication/SignIn/index.tsx
@@ -1,16 +1,22 @@
-import { Box, Button, Card, CardActions, CardContent, TextField } from '@mui/material';
+import { Box, Button, Card, CardActions, CardContent, TextField, Typography } from '@mui/material';
 import axios from 'axios';
 import React, { useState } from 'react'
 import { useCookies } from 'react-cookie';
 import { useUserStore } from '../stores';
 
-export default function SignIn() {
+interface Props {
+    setAuthView: (authView: boolean) => void,
+}
+
+export default function SignIn(props: Props) {
     const [userEmail, setUserEmail] = useState<string>('');
     const [userPassword, setUserPassword] = useState<string>('');
 
     const [cookies, setCookies] = useCookies();
-    const {user, setUser} = useUserStore();
+    const { user, setUser } = useUserStore();
 
+    const { setAuthView } = props;
+    
     // POST 로그인 정보 to backend
     const signInHandler = () => {
         if (userEmail.length === 0 || userPassword.length === 0) {
@@ -27,17 +33,17 @@ export default function SignIn() {
             .then((response) => {
                 const responseData = response.data;
                 console.log(responseData.data)
-                if (!response.data.result){
+                if (!response.data.result) {
                     alert('로그인에 실패했습니다.');
                     return;
                 }
 
                 // Make Cookie
-                const {token, exprTime, user} = responseData.data;
+                const { token, exprTime, user } = responseData.data;
                 const expires = new Date();
-                expires.setMilliseconds(expires.getMilliseconds + exprTime);
+                expires.setMilliseconds(expires.getMilliseconds() + exprTime);
 
-                setCookies('token', token, {expires});
+                setCookies('token', token, { expires });
                 setUser(user);
             })
             .catch((error) => {
@@ -47,8 +53,11 @@ export default function SignIn() {
 
     // 로그인 UI
     return (
-        <Card sx={{ minWidth: 275, maxWidth: "50vw" }}>
-            { user != null && (<>{user.userNickname}</>)}
+        <Card sx={{ minWidth: 275, maxWidth: "50vw", padding: 5 }}>
+            <Box>
+                <Typography variant='h5'>로그인</Typography>
+            </Box>
+            {user != null && (<>{user.userNickname}</>)}
             <CardContent>
                 <Box>
                     <TextField fullWidth label="이메일 주소" type="email" variant="standard" onChange={(e) => setUserEmail(e.target.value)} />
@@ -56,8 +65,13 @@ export default function SignIn() {
                 </Box>
             </CardContent>
             <CardActions>
-                <Button fullWidth onClick={() => signInHandler()} variant="contained">로그인</Button>    
+                <Button fullWidth onClick={() => signInHandler()} variant="contained">로그인</Button>
             </CardActions>
+            <Box component ='div' display='flex' mt={2}>
+                <Typography ml={1} onClick={() => setAuthView(true)}>
+                    회원가입
+                </Typography>
+            </Box>
         </Card>
     )
 }
--- mili-stock/src/views/Authentication/index.tsx
+++ mili-stock/src/views/Authentication/index.tsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SignUp from './SignUp'
 import SignIn from './SignIn'
 import { Box } from '@mui/material'
 
 export default function Authentication() {
+  // authView: true - signUp / false - signIn
+  const [authView, setAuthView] = useState<boolean>(false);
+
   return (
     <>
       <Box display='flex'>
-        <SignUp />
+        {authView ? (<SignUp />) : (<SignIn />)}
+        
 
-        <SignIn />
+        
       </Box>
     </>
   )
+
 }
